5d1daa5b5abe37a9a576dc028f8f5d26822d1bb7
Karthikeya0867 <karthikeyapandrawada@gmail.com>
2025-09-19 17:06:31 +0530

    Improve SignUp Page

--- blog-verse/frontend/src/Components/NavBar.jsx
+++ blog-verse/frontend/src/Components/NavBar.jsx
@@ -1,8 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHome } from '@fortawesome/free-solid-svg-icons';
 const NavBar = () => {
     return (
-        <nav className="flex justify-around h-16 items-center border-[lightgrey] border-b-1">
-            <h1 className="text-xl font-bold cursor-pointer">BlogVerse</h1>
-            <button className="text-blue-500 cursor-pointer hover:bg-blue-200 px-5 py-2 rounded-xl">Home</button>
+        <nav className="flex bg-white justify-evenly h-16 fixed top-0 left-0 right-0 items-center border-[lightgrey] border-b-1">
+            <div className="flex items-center">
+                <img src="./blog-logo.png" alt="logo" className="w-15 h-15 rounded-4xl"/>
+                <h1 className="text-xl font-bold cursor-pointer">BlogVerse</h1>
+            </div>
+            <div className='relative flex items-center'>
+            <FontAwesomeIcon icon={faHome} className='text-blue-500'/>
+            <button className=" text-blue-700 cursor-pointer hover:bg-gray-100 px-6 py-2  rounded-xl">Home</button>
+            </div>
             <div className="flex gap-5">
                 <button className="font-semibold text-gray-700 cursor-pointer">Sign In</button>
                 <button className="bg-blue-600 text-white font-semibold px-5 py-2 rounded-2xl cursor-pointer">Sign Up</button>
--- blog-verse/frontend/src/Components/SignUp.jsx
+++ blog-verse/frontend/src/Components/SignUp.jsx
@@ -1,43 +1,66 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faUser, faEnvelope, faLock, faEye} from '@fortawesome/free-solid-svg-icons';
 const SignUp = () => {
     return (
-        <div className="flex flex-col items-center mt-15 gap-5">
-            <h1 className="text-4xl text-blue-500 font-bold">Join BlogVerse</h1>
-            <p className="text-xl text-gray-700 text-center font-semibold">Create your account and <br /> start your Blogging journey today</p>
-            <form className="flex flex-col rounded-2xl gap-5  w-1/4 shadow-2xl shadow-purple-500 py-7 items-center">
-                <h1 className="text-3xl text-purple-500 font-bold">Sign Up</h1>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Full Name</p>
-                    <input type="text" placeholder="Enter Your Full Name"
-                        className="border-1 border-gray-500 w-full p-3 rounded-xl focus:outline-none
-                    focus:border-purple-500" />
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Email</p>
-                    <input type="email" placeholder="Enter Your Email"
-                        className="border-1 border-gray-500 w-full p-3 rounded-xl focus:outline-none
-                    focus:border-purple-500" />
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Password</p>
-                    <input type="password" placeholder="Enter Your Password"
-                        className="border-1 border-gray-500 w-full p-3 rounded-xl focus:outline-none
-                    focus:border-purple-500" />
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Confirm Password</p>
-                    <input type="password" placeholder="Confirm Your Password"
-                        className="border-1 border-gray-500 w-full p-3 rounded-xl focus:outline-none
-                    focus:border-purple-500" />
-                </div>
-                <div className="flex gap-3 w-[90%] py-3 px-4 items-center justify-center">
-                    <input type="checkbox" className="h-4 w-4" />
-                    <p>I Agree to Terms Of Service & Privacy Policy</p>
-                </div>
-                <button className="w-[90%] text-white rounded-3xl bg-purple-500 font-semibold py-4">Create Account</button>
-                <div className="border-b-1 border-[lightgray] w-[90%] mt-1" />
-                <p className="text-gray-500 font-semibold">Already Have An Account? <span className="cursor-pointer text-purple-500">Sign In Here</span></p>
-                <button className="text-gray-700 font-semibold w-[90%] py-4 cursor-pointer">Back To Home</button>
-            </form>
+        <div className="min-h-screen w-full bg-gradient-to-br from-purple-50 via-indigo-50 to-blue-50">
+            <div className="flex flex-col items-center p-18  gap-5">
+                <h1 className="text-4xl text-blue-600 font-bold">Join BlogVerse</h1>
+                <p className="text-xl text-gray-500 text-center font-normal ">Create your account and <br /> start your Blogging journey today</p>
+                <form className="flex flex-col rounded-2xl gap-6 bg-white  w-1/4 shadow-2xl  py-7 items-center">
+                    <div className="w-[90%] flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Full Name</p>
+                        <div className='relative'>
+                            <FontAwesomeIcon icon={faUser} className='absolute top-4 left-3' />
+                            <input type="text" placeholder="Enter Your Full Name"
+                                className="border-1 border-gray-300 w-full py-3 px-10 rounded-xl focus:outline-none
+                      focus:border-purple-500" />
+                        </div>
+                    </div>
+                    <div className="w-[90%] flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Email</p>
+                        <div className='relative'>
+                            <FontAwesomeIcon icon={faEnvelope} className='absolute left-3 top-4' />
+                            <input type="email" placeholder="Enter Your Email"
+                                className="border-1 border-gray-300 w-full py-3 px-10 rounded-xl focus:outline-none
+                      focus:border-purple-500" />
+                        </div>
+                    </div>
+                    <div className="w-[90%] flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Password</p>
+                        <div className="relative">
+                            <FontAwesomeIcon icon={faLock} className='absolute left-3 top-4' />
+                            <input type="password" placeholder="Enter Your Password"
+                                className="border-1 border-gray-300 w-full py-3 px-10 rounded-xl focus:outline-none
+                      focus:border-purple-500" />
+                            <FontAwesomeIcon icon={faEye} className='absolute right-3 top-4' />
+                        </div>
+                    </div>
+                    <div className="w-[90%] flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Confirm Password</p>
+                        <div className="relative">
+                        <FontAwesomeIcon icon={faLock} className='absolute left-3 top-4' />
+                        <input type="password" placeholder="Confirm Your Password"
+                            className="border-1 border-gray-300 w-full py-3 px-10 rounded-xl focus:outline-none
+                      focus:border-purple-500" />
+                        <FontAwesomeIcon icon={faEye} className='absolute right-3 top-4' />
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-2 w-[90%] rounded-xl p-4 items-center bg-gray-50 border border-gray-200">
+                        <input type="checkbox" className="h-4 w-4" />
+                        <p className="text-sm">I agree to the <span className="text-purple-500 hover:underline font-semibold">Terms of Service</span> and <span className="text-purple-500 hover:underline font-semibold">Privacy Policy</span></p>
+                    </div>
+                    <button className="w-[90%] text-white rounded-3xl bg-purple-500 font-semibold py-4">
+                        <FontAwesomeIcon icon={faUser} />
+                        Create Account
+                    </button>
+                    <div className="border-b-1 border-[lightgray] w-[90%] mt-1" />
+                    <p className="text-gray-500 font-semibold">
+                        Already Have An Account?
+                        <span className="cursor-pointer text-purple-500"> Sign In Here</span>
+                    </p>
+                    <button className="text-gray-700 bg-gray-100 rounded-xl font-semibold w-fit py-3 px-6 cursor-pointer"><FontAwesomeIcon icon={faArrowLeft} /> Back To Home</button>
+                </form>
+            </div>
         </div>
     )
 }
